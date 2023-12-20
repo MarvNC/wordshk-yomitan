8272fa81178dd36ae47cd7e4f9589c5776aafe93
MarvNC <rotrobmin@gmail.com>
2023-12-20 15:09:40 -0800

    Add script to download and unzip files

---
+++ download_unzip.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Define the directory name
+csv_dir="csvs"
+
+# Create the directory if it doesn't exist
+mkdir -p "$csv_dir"
+
+# Read each line in the file and process
+while IFS= read -r url || [ -n "$url" ]; do
+  # Ignore empty lines
+  if [ -n "$url" ]; then
+    # Extract filename from URL
+    filename=$(basename "$url")
+
+    # Check if the file or extracted file already exists
+    if [ -f "$csv_dir/$filename" ] || [ -f "$csv_dir/${filename%.*}" ]; then
+      echo "File $filename already exists. Skipping..."
+      continue
+    fi
+
+    # Download the file
+    echo "Downloading $filename..."
+    wget "$url" -O "$csv_dir/$filename"
+
+    # Unzip the downloaded file
+    echo "Unzipping $filename..."
+    gzip -d "$csv_dir/$filename"
+  fi
+done <csv.gz.URLs.txt
