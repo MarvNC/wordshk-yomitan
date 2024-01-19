467faecd0a436f4d05cbbf5350ba9ebb1096f2de
MarvNC <rotrobmin@gmail.com>
2024-01-18 20:34:44 -0800

    Basic tests

--- src/test/parseEntry.test.js
+++ src/test/parseEntry.test.js
@@ -6,9 +6,36 @@ import { parseCSVEntries } from '../util/parseCsvEntries.js';
 const testCsvFile = 'src/test/testdata.csv';
 
 /**
- * @type {DictionaryEntry[] | undefined}
+ * @type {DictionaryEntry[]}
  */
 let entries;
+
+const testCases = {
+  101613: {
+    id: 101613,
+    headwords: [
+      {
+        text: '大電',
+        reading: 'daai6 din6',
+      },
+    ],
+    tags: [
+      {
+        name: 'pos',
+        value: '名詞',
+      },
+    ],
+    glosses: [
+      {
+        explanation: {
+          yue: ['yue:D電池（量詞：粒'],
+          eng: ['eng:D cells battery'],
+        },
+        examples: [],
+      },
+    ],
+  },
+};
 
 test.before(async () => {
   entries = await parseCSVEntries(testCsvFile);
@@ -17,3 +44,10 @@ test.before(async () => {
 test('entries is defined', (t) => {
   t.not(entries, undefined);
 });
+
+for (const [id, expected] of Object.entries(testCases)) {
+  test(`Entry ${id} matches expected`, (t) => {
+    const entry = entries.find((entry) => entry.id === Number(id));
+    t.deepEqual(entry, expected);
+  });
+}
