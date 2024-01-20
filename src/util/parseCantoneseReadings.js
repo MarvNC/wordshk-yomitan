518dcfdac6307e9b43458a1f44e95ec19a3258ea
MarvNC <rotrobmin@gmail.com>
2024-01-19 16:50:12 -0800

    Add base functions for text conversion

---
+++ src/util/parseCantoneseReadings.js
@@ -0,0 +1,12 @@
+/**
+ * Parses a text string into an array matching each character to the readings
+ * @example "你get唔get到我講咩？ (nei5 get1 m4 get1 dou2 ngo5 gong2 me1?)" => [{text: "你", reading: "nei5"}, {text: "get", reading: "get1"}, ...]
+ * @param {string} text
+ * @returns {{text: string, reading: string}[]}
+ */
+function parseCantoneseReadings(text) {
+  // TODO
+  return [];
+}
+
+export { parseCantoneseReadings };
---
+++ src/util/parseTextToSC.js
@@ -0,0 +1,28 @@
+/**
+ * Parses a text string into a structured content object.
+ * @param {string} text
+ * @returns {import("yomichan-dict-builder/dist/types/yomitan/termbank").StructuredContent}
+ */
+function convertTextToSC(text, language) {
+  if (language !== 'yue') {
+    return text;
+  }
+  // /**
+  //  * @type {import("yomichan-dict-builder/dist/types/yomitan/termbank").StructuredContent}
+  //  */
+  // const sc = {
+
+  // };
+  // return sc;
+  return '';
+}
+
+/**
+ * Parses a text string into a structured content object.
+ * @param {string} text
+ * @returns {import("yomichan-dict-builder/dist/types/yomitan/termbank").StructuredContent}
+ */
+function convertCantoneseStringToSC(text) {
+  // TODO
+  return '';
+}
