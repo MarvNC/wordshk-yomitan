cc7c399eaf261cb4f6ba728149696ec10d6a7c14
MarvNC <rotrobmin@gmail.com>
2024-01-20 14:12:28 -0800

    Add ruby text

--- src/util/yomitan/parseTextToSC.js
+++ src/util/yomitan/parseTextToSC.js
@@ -2,22 +2,43 @@ import { parseCantoneseReadings } from '../textHandling/parseCantoneseReadings.j
 
 /**
  * Parses a text string into a structured content object.
- * @param {string} text
+ * @param {string} rawText
+ * @param {string} languageCode
  * @returns {import("yomichan-dict-builder/dist/types/yomitan/termbank").StructuredContent}
  */
-function convertTextToSC(text, language) {
-  if (language !== 'yue') {
-    return text;
+function convertTextToSC(rawText, languageCode) {
+  const rubyTextLangs = ['yue', 'zho', 'lzh'];
+  if (!rubyTextLangs.includes(languageCode)) {
+    return rawText;
   }
-  // /**
-  //  * @type {import("yomichan-dict-builder/dist/types/yomitan/termbank").StructuredContent}
-  //  */
-  // const sc = {
-  //   tag: 'ruby',
-  //   content: [],
-  // };
-  // return sc;
-  return '';
+  // Parse brackets for possible reading
+  const bracketRegex = /(.+)\(([^\(\)]+)\)$/;
+  const [_, phrase, reading] = rawText.match(bracketRegex) || [];
+
+  if (!phrase || !reading) {
+    return rawText;
+  }
+
+  // If reading doesn't have alphanumeric characters, it's not a jyut reading
+  const hasEnglishChars = /[a-zA-Z0-9]/.test(reading);
+  if (!hasEnglishChars) {
+    return rawText;
+  }
+
+  try {
+    const readings = parseCantoneseReadings(cleanRawText(phrase), reading);
+    return readings.map(convertReadingToRubySC);
+  } catch (error) {
+    return rawText;
+  }
+}
+
+/**
+ * Strips out # and spaces from raw text
+ * @param {string} rawText
+ */
+function cleanRawText(rawText) {
+  return rawText.replace(/#| /g, '');
 }
 
 /**
