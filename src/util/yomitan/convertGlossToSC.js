668f6864a25561df9476fabfd7955b9c8b779360
MarvNC <rotrobmin@gmail.com>
2024-01-19 23:10:43 -0800

    Basic outline for gloss function

--- src/util/yomitan/convertGlossToSC.js
+++ src/util/yomitan/convertGlossToSC.js
@@ -1,4 +1,10 @@
 import { languages } from '../../constants';
+import { isStringSentence } from '../textHandling/textUtils';
+
+const examplePhrase = '配詞 / 用法';
+const exampleSentence = '例句';
+const examplePhraseEmoji = '💬';
+const exampleSentenceEmoji = '📝';
 
 /**
  * Converts a gloss to structured content.
@@ -6,26 +12,80 @@ import { languages } from '../../constants';
  * @returns {import('yomichan-dict-builder/dist/types/yomitan/termbank').StructuredContent}
  */
 function convertGlossToSC(gloss) {
-  // TODO
-  return '';
+  /**
+   * @type {LanguageData[]}
+   */
+  const phrases = [];
+  /**
+   * @type {LanguageData[]}
+   */
+  const sentences = [];
+  for (const eg of gloss.egs) {
+    // Check if any of the language datas are a sentence
+    const isEgSentence = Object.values(eg).some((languageData) => {
+      return languageData.some((languageText) => {
+        return isStringSentence(languageText);
+      });
+    });
+    if (isEgSentence) {
+      sentences.push(eg);
+    } else {
+      phrases.push(eg);
+    }
+  }
+  return {
+    tag: 'ul',
+    data: {
+      wordshk: 'gloss',
+    },
+    lang: 'yue',
+    content: [
+      {
+        tag: 'div',
+        data: {
+          wordshk: 'definition',
+        },
+        content: convertLanguageDataToUlSC(gloss.explanation, true),
+      },
+      {
+        tag: 'div',
+        data: {
+          wordshk: 'phrase',
+        },
+        content: phrases.map((phrase) => {
+          return convertLanguageDataToUlSC(phrase, true);
+        }),
+      },
+      {
+        tag: 'div',
+        data: {
+          wordshk: 'sentence',
+        },
+        content: sentences.map((sentence) => {
+          return convertLanguageDataToUlSC(sentence, false);
+        }),
+      },
+    ],
+  };
 }
 
 /**
- * Converts one single languageData to structured content representing a definition/example/sentence.
+ * Converts one single languageData to structured content representing a definition/example/sentence as an unordered list.
  * @param {LanguageData} languageData
  * @param {boolean} isExplanation whether the languageData is an explanation
  or an example
  * @returns {import('yomichan-dict-builder/dist/types/yomitan/termbank').StructuredContent}
  */
-function convertLanguageDataToSC(languageData, isExplanation) {
+function convertLanguageDataToUlSC(languageData, isExplanation) {
   /**
    * @type {import('yomichan-dict-builder/dist/types/yomitan/termbank').StructuredContent[]}
    */
   const languageLiScArray = [];
 
-  for (const language in languageData) {
+  for (const language of Object.keys(languageData)) {
     languageLiScArray.push(
       ...convertLanguageEntryToLi(
+        // @ts-ignore
         language,
         languageData[language],
         isExplanation
@@ -49,7 +109,7 @@ function convertLanguageDataToSC(languageData, isExplanation) {
 
 /**
  * Converts a single language entry to a li item
- * @param {string} language
+ * @param {Language} language
  * @param {string[]} languageTexts
  * @param {boolean} isExplanation
  * @returns {import('yomichan-dict-builder/dist/types/yomitan/termbank').StructuredContent[]}
@@ -64,16 +124,41 @@ function convertLanguageEntryToLi(language, languageTexts, isExplanation) {
    * @type {string[]}
    */
   for (const languageText of languageTexts) {
-    languageLiScArray.push({
-      tag: 'li',
-      lang: languageInfo.langCode,
-      style: {
-        listStyleType: `"(${
-          isExplanation ? languageInfo.name : languageInfo.shortName
-        })"`,
+    // Span tag for language text
+    /**
+     * @type {import('yomichan-dict-builder/dist/types/yomitan/termbank').StructuredContent}
+     */
+    const textSpan = {
+      tag: 'span',
+      data: {
+        wordshk: 'langtext',
       },
       // TODO: use textParser function when built
       content: languageText,
+    };
+    // Make text larger for selected languages
+    const cjkLangs = ['yue', 'zho', 'jpn', 'kor', 'lzh'];
+    if (cjkLangs.includes(language) && !isExplanation) {
+      textSpan.style = {
+        fontSize: '120%',
+      };
+    }
+    languageLiScArray.push({
+      tag: 'li',
+      lang: languageInfo.langCode,
+      content: [
+        // Span tag for language name/abbreviation
+        {
+          tag: 'span',
+          data: {
+            wordshk: 'lang',
+          },
+          content: `<${
+            isExplanation ? languageInfo.name : languageInfo.shortName
+          }>`,
+        },
+        textSpan,
+      ],
       data: {
         wordshk: languageInfo.langCode,
       },
@@ -82,3 +167,5 @@ function convertLanguageEntryToLi(language, languageTexts, isExplanation) {
 
   return languageLiScArray;
 }
+
+export { convertGlossToSC, convertLanguageDataToUlSC };
